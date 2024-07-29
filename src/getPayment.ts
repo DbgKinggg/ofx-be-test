6632444ecea150fc21ec03c8cb5820d453a44abd
Samuel Chen <samuelyimingchen@gmail.com>
2024-07-29 23:15:25 +1000

    fix: use buildResponse for the getPayment endpoint for consistency

--- src/getPayment.ts
+++ src/getPayment.ts
@@ -1,37 +1,26 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 import { getPayment } from './lib/payments';
+import { buildResponse } from './lib/apigateway';
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // consider to use zod to validate the input if more parameters are added
     const paymentId = event.pathParameters?.id;
 
     if (paymentId === undefined) {
-        return {
-            statusCode: 400,
-            body: JSON.stringify({ message: 'Missing payment ID' }),
-        };
+        return buildResponse(400, { message: 'Missing payment ID' });
     }
 
     try {
         const payment = await getPayment(paymentId);
 
         if (!payment) {
-            return {
-                statusCode: 404,
-                body: JSON.stringify({ message: 'Payment not found' }),
-            };
+            return buildResponse(404, { message: 'Payment not found' });
         }
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify(payment),
-        };
+       return buildResponse(200, payment);
     } catch (error) {
         // We should log the error to CloudWatch for debugging, but for now, we'll just log to the console
         console.error('Unexpected error:', error);
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ message: 'Internal Server Error' }),
-        };
+        return buildResponse(500, { message: 'Internal Server Error' });
     }
 };
