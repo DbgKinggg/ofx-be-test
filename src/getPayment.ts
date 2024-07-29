d4923daae1ce86fc018385c0a25c9795a3e0f395
Samuel Chen <samuelyimingchen@gmail.com>
2024-07-29 22:02:10 +1000

    feat: implement getPayment handler

--- src/getPayment.ts
+++ src/getPayment.ts
@@ -1,5 +1,20 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { getPayment } from './lib/payments';
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-    throw Error('Implement me!');
+    const paymentId = event.pathParameters?.id;
+
+    if (paymentId === undefined) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Missing payment ID' }),
+        };
+    }
+    
+    const payment = await getPayment(paymentId);
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(payment),
+    };
 };
